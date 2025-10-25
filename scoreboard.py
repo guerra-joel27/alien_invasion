796fc1e468d29b141f9253ec38eeffd84ba31455
Joel Guerra <joel.guerra@mavs.uta.edu>
2025-10-25 16:14:39 -0500

    high score drawn on screen

--- scoreboard.py
+++ scoreboard.py
@@ -51,3 +51,4 @@ class Scoreboard:
     def show_score(self):
         """draw the score to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
